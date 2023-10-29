01c9cca9319acf4e3871696813355a1377527b57
Uğur Akçay <akcayugur01@gmail.com>
2023-10-29 23:02:07 +0300

    aa

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -61,8 +61,9 @@ export default function Navbar() {
   return (
     <>
       <div
-        className={`fixed top-0 w-full flex justify-center font-inter text-[color:var(--color-primary)] bg-transparent  transition-translate duration-200 text-xl ${scrollY > 10 ? "translate-y-6 " : ""
-          }`}
+        className={`fixed top-0 w-full flex justify-center font-inter text-[color:var(--color-primary)] bg-transparent  transition-translate duration-200 text-xl ${
+          scrollY > 10 ? "translate-y-6 " : ""
+        }`}
       >
         <div
           className={classNames(
@@ -87,100 +88,102 @@ export default function Navbar() {
             <div className="flex items-center gap-x-2">
               {mainMenu &&
                 mainMenu.map((menuItem, index) => (
-                  <>
-                    <div className="flex-col relative justify-center items-center p-2 bg-red-100 group">
-                      <div className="flex items-center gap-x-2">
-                        <div>
-                          {menuItem.name}
+                  // <>
+                  //   <div key={index} className="flex-col relative justify-center items-center p-2 bg-red-100 group">
+                  //     <div className="flex items-center gap-x-2">
+                  //       <div>
+                  //         {menuItem.name}
 
-                        </div>
-                        <div>
-                          {menuItem?.icon && (
-                            <>
-                              <svg
-                                height="6"
-                                viewBox="0 0 12 6"
-                                width="12"
-                                xmlns="http://www.w3.org/2000/svg"
-                              >
-                                <path
-                                  fill="currentColor"
-                                  d="m9.21966991 5.78033009c.29289322.29289321.76776696.29289321 1.06066019 0 .2928932-.29289322.2928932-.76776696 0-1.06066018l-4.50000001-4.5c-.29289322-.29289321-.76776696-.29289321-1.06066018 0l-4.5 4.5c-.29289321.29289322-.29289321.76776696 0 1.06066018.29289322.29289321.76776696.29289321 1.06066018 0l3.96966991-3.96966992z"
-                                  transform="matrix(-1 0 0 -1 11.25 6)"
-                                ></path>
-                              </svg></>
-                          )}
-                        </div>
-                      </div>
-                      {menuItem?.subtitle && (
-                        <>
-                          <div className=" pt-10 absolute max-h-[40em] left-1/2 top-11 -translate-x-1/2  border border-yellow-500 invisible group-hover:visible">
-                            <div className="w-[700px] flex flex-start items-center flex-wrap py-[1.25rem] px-[1.5rem]  opacity-0 transition-opacity  border border-black rounded-xl group-hover:flex  group-hover:opacity-100 ">
-                              <div className="w-[calc(50%-1.25rem)] m-[0.625rem] border border-black hover:bg-[#e6ebee] rounded-xl px-0">
-                                <a href="" className="rounded-2xl block h-full py-[0.8rem] px-[1.25rem] relative text-left  ">
-                                  <div className="mb-[0.3rem] text-bold text-lg font-poppins text-[#232e35]">deneme</div>
-                                  <p className="text-[#232e35] text-[0.875rem] ">pppp11111</p>
-                                </a>
-
-                              </div>
-                              <div className="w-[calc(50%-1.25rem)] m-[0.625rem] border border-black">
-                                <a href="" className="rounded-2xl block h-full py-[0.8rem] px-[1.25rem] relative text-left  ">
-                                  <div className="mb-[0.3rem] text-[#656d72] ">divvv222</div>
-                                  <p className="text-[#232e35] text-[0.875rem] ">pppp222</p>
-                                </a>
-                              </div>
-
-                            </div>
-
-                          </div>
-                        </>
-                      )}
-
-                    </div>
-                  </>
-                  //   <button
-                  //     key={index}
-                  //     className=" relative flex items-center justify-center font-serif gap-x-2 p-2 mobile:px-1 laptop1:px-2 desktop1:px-3  text-base group"
-                  //   >
-                  //     <span className="whitespace-nowrap before:absolute before:bottom-0 before:content-[''] before:w-0 before:-translate-x-1/2 before:-translate-y-1/2 before:left-1/2 before:h-1 before:bg-golden group-hover:before:w-full before:rounded-3xl before:transition-all before:duration-300 before:ease-in-out">
-                  //       {menuItem.name}
-                  //     </span>
-                  //     { menuItem?.subtitle && (
+                  //       </div>
+                  //       <div>
+                  //         {menuItem?.icon && (
+                  //           <>
+                  //             <svg
+                  //               height="6"
+                  //               viewBox="0 0 12 6"
+                  //               width="12"
+                  //               xmlns="http://www.w3.org/2000/svg"
+                  //             >
+                  //               <path
+                  //                 fill="currentColor"
+                  //                 d="m9.21966991 5.78033009c.29289322.29289321.76776696.29289321 1.06066019 0 .2928932-.29289322.2928932-.76776696 0-1.06066018l-4.50000001-4.5c-.29289322-.29289321-.76776696-.29289321-1.06066018 0l-4.5 4.5c-.29289321.29289322-.29289321.76776696 0 1.06066018.29289322.29289321.76776696.29289321 1.06066018 0l3.96966991-3.96966992z"
+                  //                 transform="matrix(-1 0 0 -1 11.25 6)"
+                  //               ></path>
+                  //             </svg></>
+                  //         )}
+                  //       </div>
+                  //     </div>
+                  //     {menuItem?.subtitle && (
                   //       <>
+                  //         <div className=" pt-10 absolute max-h-[40em] left-1/2 top-11 -translate-x-1/2  border border-yellow-500 invisible group-hover:visible">
+                  //           <div className="w-[700px] flex flex-start items-center flex-wrap py-[1.25rem] px-[1.5rem]  opacity-0 transition-opacity  border border-black rounded-xl group-hover:flex  group-hover:opacity-100 ">
+                  //             <div className="w-[calc(50%-1.25rem)] m-[0.625rem] border border-black hover:bg-[#e6ebee] rounded-xl px-0">
+                  //               <a href="" className="rounded-2xl block h-full py-[0.8rem] px-[1.25rem] relative text-left  ">
+                  //                 <div className="mb-[0.3rem] text-bold text-lg font-poppins text-[#232e35]">deneme</div>
+                  //                 <p className="text-[#232e35] text-[0.875rem] ">pppp11111</p>
+                  //               </a>
 
-                  //         <svg
-                  //           height="6"
-                  //           viewBox="0 0 12 6"
-                  //           width="12"
-                  //           xmlns="http://www.w3.org/2000/svg"
-                  //         >
-                  //           <path
-                  //             fill="currentColor"
-                  //             d="m9.21966991 5.78033009c.29289322.29289321.76776696.29289321 1.06066019 0 .2928932-.29289322.2928932-.76776696 0-1.06066018l-4.50000001-4.5c-.29289322-.29289321-.76776696-.29289321-1.06066018 0l-4.5 4.5c-.29289321.29289322-.29289321.76776696 0 1.06066018.29289322.29289321.76776696.29289321 1.06066018 0l3.96966991-3.96966992z"
-                  //             transform="matrix(-1 0 0 -1 11.25 6)"
-                  //           ></path>
-                  //         </svg>
-                  //         <div className="pt-[2.8rem] px-[1.25rem] bg-red-500 opacity-100  visible top-24">
-                  //           <div className="w-[700px]  top-12 rounded-xl gap-x-4 px-[1.25rem] py-[1.25rem] bg-[#fff] absolute hidden left-1/2 -translate-x-1/2 transition-all duration-700 opacity-0 group-hover:flex  group-hover:opacity-100 group-hover:pointer-events-auto border border-black">
-                  //             <div className="grid grid-cols-2  ">
-                  //               {Object.keys(menuItem.subtitle).map((contentKey, index) => (
-                  //                 // Her bir "contentKey" için bu döngü çalışacak
-                  //                 <div key={index} className="w-[321px] flex flex-col items-start justify-start text-justify  flex-wrap p-3 hover:bg-[#e6ebee] rounded-xl px-7">
-                  //                   <h2 className="text-bold text-lg font-poppins text-[#232e35] ">
-                  //                     {menuItem.subtitle[contentKey].title}
-                  //                   </h2>
-                  //                   <p className="text-[#656d72] text-[0.875rem]">
-                  //                     {menuItem.subtitle[contentKey].comment}
-                  //                   </p>
-                  //                 </div>
-                  //               ))}
                   //             </div>
+                  //             <div className="w-[calc(50%-1.25rem)] m-[0.625rem] border border-black">
+                  //               <a href="" className="rounded-2xl block h-full py-[0.8rem] px-[1.25rem] relative text-left  ">
+                  //                 <div className="mb-[0.3rem] text-[#656d72] ">divvv222</div>
+                  //                 <p className="text-[#232e35] text-[0.875rem] ">pppp222</p>
+                  //               </a>
+                  //             </div>
+
                   //           </div>
+
                   //         </div>
                   //       </>
                   //     )}
-                  //   </button>
 
+                  //   </div>
+                  // </>
+                  <button
+                    key={index}
+                    className=" relative flex items-center justify-center font-serif gap-x-2 p-2 mobile:px-1 laptop1:px-2 desktop1:px-3  text-base group"
+                  >
+                    <span className="whitespace-nowrap before:absolute before:bottom-0 before:content-[''] before:w-0 before:-translate-x-1/2 before:-translate-y-1/2 before:left-1/2 before:h-1 before:bg-golden group-hover:before:w-full before:rounded-3xl before:transition-all before:duration-300 before:ease-in-out">
+                      {menuItem.name}
+                    </span>
+                    {menuItem?.subtitle && (
+                      <>
+                        <svg
+                          height="6"
+                          viewBox="0 0 12 6"
+                          width="12"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            fill="currentColor"
+                            d="m9.21966991 5.78033009c.29289322.29289321.76776696.29289321 1.06066019 0 .2928932-.29289322.2928932-.76776696 0-1.06066018l-4.50000001-4.5c-.29289322-.29289321-.76776696-.29289321-1.06066018 0l-4.5 4.5c-.29289321.29289322-.29289321.76776696 0 1.06066018.29289322.29289321.76776696.29289321 1.06066018 0l3.96966991-3.96966992z"
+                            transform="matrix(-1 0 0 -1 11.25 6)"
+                          ></path>
+                        </svg>
+
+                        <div className="w-[700px] top-24 group-hover:top-16 rounded-xl gap-x-4 px-[1.25rem] py-[1.25rem] bg-[#fff] absolute invisible group-hover:visible left-1/2 -translate-x-1/2 transition-all duration-500 opacity-0   group-hover:opacity-100  border border-black">
+                          <div className="grid grid-cols-2  ">
+                            {Object.keys(menuItem.subtitle).map(
+                              (contentKey, index) => (
+                                // Her bir "contentKey" için bu döngü çalışacak
+                                <div
+                                  key={index}
+                                  className="w-[321px] flex flex-col items-start justify-start text-justify  flex-wrap p-3 hover:bg-[#e6ebee] rounded-xl px-7"
+                                >
+                                  <h2 className="text-bold text-lg font-poppins text-[#232e35] ">
+                                    {menuItem.subtitle[contentKey].title}
+                                  </h2>
+                                  <p className="text-[#656d72] text-[0.875rem]">
+                                    {menuItem.subtitle[contentKey].comment}
+                                  </p>
+                                </div>
+                              )
+                            )}
+                          </div>
+                        </div>
+                      </>
+                    )}
+                  </button>
                 ))}
               <button className="whitespace-nowrap font-bold bg-golden  rounded-full text-white mobile:text-base mobile:py-0 mobile:px-4  desktop1:py-2 desktop1:px-5 ">
                 Sign up Free
--- src/const/navbarMenu.jsx
+++ src/const/navbarMenu.jsx
@@ -8,20 +8,18 @@ export const mainMenu = [
     path: "/use-cases",
     name: "Kullanım Durumları",
     icon: true,
-    // subtitle: {
-
-    //   content_1: {
-    //     title: "Takımlar için",
-    //     comment:
-    //       "Farklı departman ve konumlardaki tüm ekibinizi tek bir hesap altında nasıl planlayabileceğinizi kontrol edin.",
-    //   },
-    //   content_2: {
-    //     title: "Eğitim için",
-    //     comment:
-    //       "Olağanüstü bir öğrenci deneyimi sunmaya odaklanabilmeniz için rezervasyonları otomatikleştirin.",
-    //   },
-
-    // },
+    subtitle: {
+      content_1: {
+        title: "Takımlar için",
+        comment:
+          "Farklı departman ve konumlardaki tüm ekibinizi tek bir hesap altında nasıl planlayabileceğinizi kontrol edin.",
+      },
+      content_2: {
+        title: "Eğitim için",
+        comment:
+          "Olağanüstü bir öğrenci deneyimi sunmaya odaklanabilmeniz için rezervasyonları otomatikleştirin.",
+      },
+    },
   },
   // {
   //     path:"/profile",
@@ -37,18 +35,18 @@ export const mainMenu = [
     path: "/product",
     name: "Ürünler",
     icon: true,
-    // subtitle: {
-    //   content_1: {
-    //     title: "Uygunluk Durumunuzu Yönetin",
-    //     comment:
-    //       "Programınızı ve toplantılarınızı yaşam tarzınıza uyacak ve işinizi kolaylaştıracak şekilde uyarlayın. Müsaitlik durumunuzu anında güncellemek için takvimlerinizi bağlayın.",
-    //   },
-    //   content_2: {
-    //     title: "For educations",
-    //     comment:
-    //       "Automate bookings so you can focus on delivering an outstanding student experience. ",
-    //   },
-    // },
+    subtitle: {
+      content_1: {
+        title: "Uygunluk Durumunuzu Yönetin",
+        comment:
+          "Programınızı ve toplantılarınızı yaşam tarzınıza uyacak ve işinizi kolaylaştıracak şekilde uyarlayın. Müsaitlik durumunuzu anında güncellemek için takvimlerinizi bağlayın.",
+      },
+      content_2: {
+        title: "For educations",
+        comment:
+          "Automate bookings so you can focus on delivering an outstanding student experience. ",
+      },
+    },
   },
   {
     path: "/company",
