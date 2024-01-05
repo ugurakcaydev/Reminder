46d9a6039dfbea11d802b2bd4bce858771670f90
Uğur Akçay <akcayugur01@gmail.com>
2024-01-05 16:44:47 +0300

    votepagebyugur

--- src/pages/VotePage/index.jsx
+++ src/pages/VotePage/index.jsx
@@ -1,116 +1,148 @@
 // VotePage component
-import React, { useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import classNames from 'classnames';
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
+import classNames from "classnames";
+import { Button } from "rsuite";
 
-//Vote Results Send 
-import { sendDataToApi } from '../../api/BookingVoteData';
-
+//Vote Results Send
+import { sendDataToApi } from "../../api/BookingVoteData";
 
 export default function VotePage() {
-    const location = useLocation();
-    const meeting = location.state?.meetingKey;
+  const location = useLocation();
+  const meeting = location.state?.meetingKey;
 
-    console.log("TOPLANTI DETAYLARI", meeting);
+  console.log("TOPLANTI DETAYLARI", meeting);
 
-    const [votingClosed, setVotingClosed] = useState(false);
-    const [selectedDays, setSelectedDays] = useState([]);
+  const [votingClosed, setVotingClosed] = useState(false);
+  const [selectedDays, setSelectedDays] = useState([]);
 
-    const handleCheckboxChange = (day) => {
-        if (!votingClosed) {
-            setSelectedDays((prevDays) => {
-                if (prevDays.includes(day)) {
-                    return prevDays.filter((d) => d !== day);
-                } else {
-                    return [...prevDays, day];
-                }
-            });
+  const handleCheckboxChange = (day) => {
+    if (!votingClosed) {
+      setSelectedDays((prevDays) => {
+        if (prevDays.includes(day)) {
+          return prevDays.filter((d) => d !== day);
+        } else {
+          return [...prevDays, day];
         }
+      });
+    }
+  };
+
+  const handleVote = async () => {
+    if (votingClosed) {
+      console.log("Ankete zaten oy verildi!");
+      return;
+    }
+
+    const meetingData = {
+      ...meeting,
+      selectedDays,
     };
+    const response = await sendDataToApi(meetingData);
+    console.log("Anket Sonuçları: ", response);
+    setVotingClosed(true);
 
-    const handleVote = async () => {
-        if (votingClosed) {
-            console.log("Ankete zaten oy verildi!");
-            return;
-        }
+    console.log("SEÇİLEN GÜNLER: ", selectedDays);
+  };
+  return (
+    <div className="w-full h-[100vh] flex items-center justify-center  p-2 text-[#fff]">
+      {meeting && (
+        <div className=" w-[700px] h-[650px] overflow-hidden rounded-xl  z-[1] relative shadow-xl shadow-[rgba(0,0,0,0.35)] ">
+          <div
+            className="flex bg-[#1d2629] h-full "
+            style={{ overflow: "hidden" }}
+          >
+            <div className="w-1/2 relative px-5 py-6 flex flex-col justify-between">
+              <div className="flex flex-col gap-y-5 w-full">
+                <div className="w-24 h-24 rounded-full flex items-center justify-center bg-[#32414a] " />
+                <div
+                  className="flex break-words font-semibold text-lg min-h-[30px] gap-x-2"
+                  style={{
+                    wordWrap: "break-word",
+                    whiteSpace: "pre-wrap",
+                    wordBreak: "break-word",
+                  }}
+                >
+                  <div>Toplantı Adı:</div>
+                  <div className="text-[#F7B22C]">{meeting.MeetingName}</div>
+                </div>
+                <div className="flex w-fit items-center justify-start gap-x-1 border px-2 py-0.5 rounded-full  border-gray-500">
+                  <svg className="w-4 h-4" viewBox="0 0 16 16">
+                    <path
+                      fill="currentColor"
+                      d="M8 0a8 8 0 110 16A8 8 0 018 0zm0 2a6 6 0 100 12A6 6 0 008 2zm0 2c.5 0 1 .4 1 .9V7h1c.5 0 1 .4 1 .9V8c0 .6-.4 1-1 1H8a1 1 0 01-1-.9V5c0-.6.4-1 1-1z"
+                    ></path>
+                  </svg>
+                  <div>
+                    {meeting.Hours == 0 ? "" : meeting.Hours + " saat "}
+                    {meeting.Minute == 0 ? "" : meeting.Minute + " dakika "}
+                  </div>
+                </div>
+                <div className="mt-2 mb-0.5 w-full text-center font-semibold text-base">
+                  Gün ve Saati Oylayınız
+                </div>
+                <div className="flex flex-col gap-y-4">
+                  {meeting.GetAllMeetingDetailDtos?.$values.map((days) => {
+                    let saat =
+                      parseInt(days.MeetingStart.split(":")[0]) + meeting.Hours;
+                    let dakika =
+                      parseInt(days.MeetingStart.split(":")[1]) +
+                      meeting.Minute;
 
-        const meetingData = {
-            ...meeting,
-            selectedDays,
-        };
-        const response = await sendDataToApi(meetingData);
-        console.log("Anket Sonuçları: ", response);
-        setVotingClosed(true);
+                    if (dakika >= 60) {
+                      saat += 1;
+                      dakika %= 60;
+                    }
 
-        console.log("SEÇİLEN GÜNLER: ", selectedDays);
-    };
+                    return (
+                      <div
+                        key={days.$id}
+                        className="w-full flex items-center justify-start gap-x-2 "
+                      >
+                        <input
+                          className="w-4 h-4"
+                          type="checkbox"
+                          onChange={() => handleCheckboxChange(days)}
+                          disabled={votingClosed}
+                        />
+                        {`${days.MeetingsDay} | ${days.MeetingStart} : ${
+                          saat < 10 ? "0" + saat : saat
+                        }:${dakika < 10 ? "0" + dakika : dakika}`}
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
 
-    return (
-        <div className='w-full h-[100vh] flex items-center justify-center  p-2 text-[#fff]'>
-            {meeting && (
-                <>
-                    <div className='w-[500px] h-[600px] flex flex-col justify-between  p-5 border rounded-2xl border-[#ffffff78] shadow-xl shadow-[rgba(0,0,0,0.3)] '>
-                        <div className='flex flex-col gap-y-7'>
-                            <section className='flex justify-between '>
-                                <div className='flex flex-col'>
-                                    <span className='text-lg'>Toplantı Adı: {meeting.MeetingName}</span>
-                                    <span className='text-sm'>Topantı Süresi: {meeting.Hours} saat {meeting.Minute} dakika</span>
-                                </div>
-                                <div className='flex flex-col'>
-                                    <span className='text-base'>Toplantı Sahibi: {meeting.UserName}</span>
-                                    <span className='text-sm'>E-mail: {meeting.Email}</span>
-                                </div>
-                            </section>
-                            <span className='text-base'>Toplantı Katılımcıları</span>
-                            <section className='max-h-[100px] overflow-x-hidden flex flex-col  '>
-                                {meeting.GetAllMeetingItemDto?.$values.map((person) => (
-                                    <li key={person.Id} className='list-decimal'>
-                                        {person.Email}
-                                    </li>
-                                ))}
-                            </section>
-                            <section className='flex flex-col gap-y-2 '>
-                                {meeting.GetAllMeetingDetailDtos?.$values.map((days) => {
-                                    let saat = parseInt(days.MeetingStart.split(':')[0]) + meeting.Hours;
-                                    let dakika = parseInt(days.MeetingStart.split(':')[1]) + meeting.Minute;
-
-                                    if (dakika >= 60) {
-                                        saat += 1;
-                                        dakika %= 60;
-                                    }
-
-                                    return (
-                                        <div key={days.$id} className='w-full flex items-center justify-start gap-x-2'>
-                                            <input
-                                                className='w-4 h-4'
-                                                type="checkbox"
-                                                onChange={() => handleCheckboxChange(days)}
-                                                disabled={votingClosed}
-                                            />
-                                            {`${days.MeetingsDay} | ${days.MeetingStart} : ${saat < 10 ? '0' + saat : saat}:${dakika < 10 ? '0' + dakika : dakika}`}
-                                            {/** Oyla butonuna tıkladıktan sonra her seçeneğin yanına yüzdelik değer gelecek */}
-                                        </div>
-                                    );
-                                })}
-                            </section>
-
-                        </div>
-                        <div className='flex justify-center '>
-                            <div className='w-full text-center'>
-                                <button
-                                    onClick={votingClosed ? undefined : handleVote}
-                                    className={classNames('w-[80%] py-3 text-base rounded-xl', {
-                                        'bg-[#777777] cursor-not-allowed': votingClosed,
-                                        'bg-[#3daf26aa] hover:bg-[#3daf26]': !votingClosed,
-                                    })}
-                                >
-                                    {votingClosed ? 'OY VERİLDİ' : 'OYLA'}
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </>
-            )}
+              <Button
+                onClick={votingClosed ? undefined : handleVote}
+                className={classNames(
+                  "px-3 py-1.5 rounded-full bg-[#F7B22C] text-base transition-all duration-500 ease-in",
+                  {
+                    "!bg-gray-500 !pointer-events-none !cursor-not-allowed":
+                      selectedDays.length === 0,
+                  }
+                )}
+              >
+                Oyla
+              </Button>
+            </div>
+            <div className="flex-1 bg-[#1d2629]  p-4 rounded-tr-3xl border-l border-l-[#F7B22C] flex flex-col gap-y-3 overflow-auto">
+              <div className="w-full text-center mb-3 text-base font-semibold text-[#F7B22C]">
+                Davet Edilen Kullanıcılar
+              </div>
+              {meeting.GetAllMeetingItemDto?.$values.map((person, i) => (
+                <div
+                  className="mb-1 w-full py-1.5 rounded-full flex items-center justify-center bg-[#32414a] font-semibold"
+                  key={i}
+                >
+                  {person.Email}
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
-    );
+      )}
+    </div>
+  );
 }
