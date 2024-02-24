04557064525ce81937845bbb843699a520bd8649
Kadir Demirkaya <126807887+kadirdemirkaya@users.noreply.github.com>
2024-02-24 15:50:54 +0300

    Update

--- src/store/appearance/actions.js
+++ src/store/appearance/actions.js
@@ -4,3 +4,4 @@ import { _setBackgroundColor, _setColor } from "../../store/appearance/index";
 export const setBackgroundColor = (data) => store.dispatch(_setBackgroundColor(data));
 export const setColor = (data) => store.dispatch(_setColor(data));
 //2
+//3
